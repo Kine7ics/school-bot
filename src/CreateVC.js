ce813ea89f56b14433e8b09636a15a8c9374be93
Kine7ics <Kinetics@github.com>
2021-03-04 19:11:45 +0100

    finished framework (Not yet tested)

--- src/CreateVC.js
+++ src/CreateVC.js
@@ -3,14 +3,17 @@ const settingsSchema = require("./schemas/settings-schema");
 
 module.exports = (client) => {
   client.on("voiceStateUpdate", async (oldState, newState) => {
-    const { guild } = newState;
-
-    let vcBuildingChannelID;
+    const { guild, channelID, channel, member } = newState;
 
     await mongo().then(async (mongoose) => {
       try {
         await settingsSchema.findOne({ _id: guild.id }).then((settings) => {
-          vcBuildingChannelID = settings.joinForVC_ID();
+          if (settings.joinForVC_ID() === channelID) {
+            guild.channels.create(member.displayName, {
+              parent: channel.parent,
+              position: channel.position + 1,
+            });
+          }
         });
       } finally {
         await mongoose.connection.close();
