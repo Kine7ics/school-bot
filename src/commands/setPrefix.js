5d7db9aaae0828c9db89e386a61b48be23d87db8
Kine7ics <Kinetics@github.com>
2021-03-04 10:48:58 +0100

    Adds the setPrefix command

--- src/commands/setPrefix.js
+++ src/commands/setPrefix.js
@@ -4,8 +4,30 @@ module.exports = {
   permissionError: "You need admin permissions to run this command",
   minArgs: 1,
   maxArgs: 1,
-  callback: (message, arguments, text) => {
-    //TODO: build this
+  callback: async (message, arguments) => {
+    const mongo = require("../mongo"),
+      settingSchema = require("../schemas/settings-schema");
+
+    const { guild } = message;
+    const newPrefix = arguments[0];
+
+    await mongo().then(async (mongoose) => {
+      try {
+        await settingSchema
+          .findOneAndUpdate(
+            { _id: guild.id },
+            { _id: guild.id, commandPrefix: newPrefix },
+            { upsert: true }
+          )
+          .then(() => {
+            message.reply(
+              `the command prefix has been changed to: "${newPrefix}"`
+            );
+          });
+      } finally {
+        await mongoose.connection.close();
+      }
+    });
   },
   permissions: ["ADMINISTRATOR"],
 };
